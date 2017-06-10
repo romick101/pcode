dace500b364ed8902427b16ee127f7b8ed641bf5
Roman Stylyk <romick101@gmail.com>
2017-06-10 18:55:32 +0300

    Added connection to mongo

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,14 @@
+const chalk = require('chalk');
+
 /**
   * @param {Object} app - express instance
   * @param {Object} db - database
   */
 
 module.exports = (app, db) => {
-  app.post('/', (req, res) => res.send('Code will be uploaded to the server'));
+  app.post('/upload', (req, res) => {
+    debugger;
+    console.log(chalk.blue('Request body: ') + JSON.stringify(req.body));
+    res.send('Code will be uploaded to the server');
+  });
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const http = require('http');
+const when = require('when');
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongo = require('mongodb').MongoClient;
+const assert = require('assert');
 const app = express();
 const routes = require('./routes');
 
@@ -7,11 +11,25 @@ class Server {
   constructor () {
     this.host = '127.0.0.1';
     this.port = 7777;
+    this.dbUrl = 'mongodb://localhost:27017/pcodedb'
+    app.use(bodyParser.urlencoded({ extended: true}));
+  }
+  connectToDB(){
+    return when.promise((resolve, reject) => {
+      mongo.connect(this.dbUrl, (err, db) => {
+        if (err !== null) reject(err);
+        console.log("Connected to Mongo server: " + this.dbUrl);
+        resolve(true);
+      });
+    });
   }
   run () {
     routes(app, {});
-    app.listen(this.port, () => console.log("Server is running on " + this.port));
-  }
+    this.connectToDB()
+      .then(_ => {
+        app.listen(this.port, () => console.log("Server is running on " + this.port));
+      });
+    }
 }
 
 (new Server()).run();
