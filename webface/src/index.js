aa6f6794e58df960a8115969e31d60d852fd5a71
Roman Stylyk <romick101@gmail.com>
2017-06-14 02:24:44 +0300

    rework webface

--- webface/src/index.js
+++
@@ -1,8 +0,0 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-const CodeForm = require("./CodeForm.js")
-const { register } = require("./registerServiceWorker");
-require("./index.css");
-
-ReactDOM.render(<CodeForm />, document.getElementById('root'));
-register();
